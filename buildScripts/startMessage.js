1265752e4e877263c21b699e5bc86f53308ce798
Li, Dongdong (NDR) <dli@ndr.com>
2017-07-24 15:34:52 -0400

    Adds babel transpiler

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
-var chalk = require('chalk');
+//var chalk = require('chalk');// old way
+
+import chalk from 'chalk';// cause error to node
 
 console.log(chalk.green('Starting app in dev mode...'));
